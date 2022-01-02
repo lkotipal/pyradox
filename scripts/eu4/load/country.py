40cd1d86fe94d939d264e0be0812592feb5b3a03
grotaclas <grotaclas@gmail.com>
2022-01-02 17:45:22 +0100

    eu4: new get_countries function

--- scripts/eu4/load/country.py
+++ scripts/eu4/load/country.py
@@ -3,10 +3,16 @@ import os
 import pyradox
 
 
-parse_countries, get_countries = pyradox.load.load_functions('EU4', 'countries', ('history', 'countries'))
-
 def get_country_name(tag):
     """
     Gets the name a country by its tag according to localisation.
     """
     return pyradox.yml.get_localisation(tag, game = 'EU4')
+
+def get_countries():
+    result = pyradox.Tree()
+    for filename, tree in pyradox.parse_dir(os.path.join('history', 'countries'), game='EU4'):
+        tag, raw_name = pyradox.format.split_filename(filename)
+        tag = tag.replace('-', '')
+        result.append(tag, tree)
+    return result
