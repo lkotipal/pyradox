07efbd3339510649cb6318f8a0c10448e52703bf
grotaclas <grotaclas@gmail.com>
2022-01-04 20:09:01 +0100

    eu4: update climate map and add weather map
    the weather map combines winter and monsoon. Provinces which have both
    are shaded with both colors. This is accomplished by generating two
    images, one which uses the winter for provinces which have both and the
    other uses monsoon for provinces which have both. These images are then
    combined by alternating taking three pixel from each image

--- scripts/eu4/climate_map.py
+++ scripts/eu4/climate_map.py
@@ -4,51 +4,78 @@ import re
 import collections
 
 import pyradox
+from PIL import Image
 
 
 color_defs = collections.OrderedDict([
-    ('default', (127, 127, 127)),
-    ('tropical' , (255, 63, 63)),       # light red
-    ('arid' , (255, 255, 127)),         # light yellow
-    ('arctic' , (127, 127, 255)),       # light blue
-
-    #('default', (0, 63, 0)),            #
-    #('mild_winter', (0, 191, 0)),       # green
-    #('normal_winter', (63, 255, 63)),   # light green
-    #('severe_winter', (191, 255, 191)), # pale green
+    ('default', (102, 127, 68)),
+    ('tropical' , (102, 178, 48)),
+    ('arid' , (216, 214, 66)),
+    ('arctic' , (255, 255, 255)),
     ])
 
-legend = ''
-for name, color in color_defs.items():
-    bg_color_string = '#%02x%02x%02x' % color
-    r, g, b = color
-    y = 0.2126 * r + 0.7152 * g + 0.0722 * b
-    if y >= 255 / 2:
-        text_color_string = '#000000'
-    else:
-        text_color_string = '#ffffff'
-    legend += '<span style="color:%s; background-color:%s">%s </span>' % (text_color_string, bg_color_string, name)
+color_defs_weather = collections.OrderedDict([
+    ('default', (0, 0, 0)),
+    ('mild_winter', (85, 85, 85)),
+    ('normal_winter', (170, 170, 170)),
+    ('severe_winter', (255, 255, 255)),
+    ('mild_monsoon', (0, 0, 85)),
+    ('normal_monsoon', (0, 0, 170)),
+    ('severe_monsoon', (0, 0, 255)),
+    ])
 
-print(legend)
+def generate_map_and_legend(color_defs, prefer_last=True):
+    legend = ''
+    for name, color in color_defs.items():
+        bg_color_string = '#%02x%02x%02x' % color
+        r, g, b = color
+        y = 0.2126 * r + 0.7152 * g + 0.0722 * b
+        if y >= 255 / 2:
+            text_color_string = '#000000'
+        else:
+            text_color_string = '#ffffff'
+        legend += '<span style="color:%s; background-color:%s">%s </span>' % (text_color_string, bg_color_string, name)
 
-climate_map = {}
-for climate, provinces in pyradox.txt.parse_file(os.path.join(pyradox.get_game_directory('EU4'), 'map', 'climate.txt'), verbose=False).items():
-    for province_id in provinces:
+    print(legend)
+
+    climate_map = {}
+    for climate, province_id in pyradox.txt.parse_file(os.path.join(pyradox.get_game_directory('EU4'), 'map', 'climate.txt'), verbose=False).items():
+        if climate == 'equator_y_on_province_image':
+            continue
         if climate in color_defs.keys():
-            climate_map[province_id] = climate
+            if prefer_last or province_id not in climate_map:
+                climate_map[province_id] = climate
 
-colormap = {}
-for filename, data in pyradox.txt.parse_dir(os.path.join(pyradox.get_game_directory('EU4'), 'history', 'provinces'), verbose=False):
-    m = re.match('\d+', filename)
-    province_id = int(m.group(0))
-    if 'base_tax' not in data: continue # skip wastelands
-    if province_id in climate_map:
-        colormap[province_id] = color_defs[climate_map[province_id]]
-    else:
-        colormap[province_id] = color_defs['default']
-        
-province_map = pyradox.worldmap.ProvinceMap()
-out = province_map.generate_image(colormap)
-out.save('out/climate_map.png')
+    colormap = {}
+    for filename, data in pyradox.txt.parse_dir(os.path.join(pyradox.get_game_directory('EU4'), 'history', 'provinces'), verbose=False):
+        m = re.match('\d+', filename)
+        province_id = int(m.group(0))
+        if 'base_tax' not in data: continue # skip wastelands
+        if province_id in climate_map:
+            colormap[province_id] = color_defs[climate_map[province_id]]
+        else:
+            colormap[province_id] = color_defs['default']
 
+    province_map = pyradox.worldmap.ProvinceMap(game = 'EU4')
+    return province_map.generate_image(colormap, default_land_color = (93, 93, 93))
 
+generate_map_and_legend(color_defs).save('out/Climate_map.png')
+
+# create a shaded image
+# first create an image where winter overrides monsoon(which is last in the climate.txt)
+# then merge the images by alternating using three pixel form each image
+prefer_monsoon_image = generate_map_and_legend(color_defs_weather)
+prefer_winter_image = generate_map_and_legend(color_defs_weather, prefer_last=False)
+
+shaded_image = Image.new(prefer_monsoon_image.mode, prefer_monsoon_image.size)
+for x in range(prefer_monsoon_image.width):
+        for y in range(prefer_monsoon_image.height):
+
+            if (x+y) % 6 < 3:
+                source_image_for_current_pixel = prefer_monsoon_image
+            else:
+                source_image_for_current_pixel = prefer_winter_image
+
+            shaded_image.putpixel((x,y), source_image_for_current_pixel.getpixel((x,y)))
+
+shaded_image.save('out/Weather_map.png')
