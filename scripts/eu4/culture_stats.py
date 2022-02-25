ddcb3cdd41e4a1c8ccacd05d6f533761b81f32d3
grotaclas <grotaclas@gmail.com>
2022-02-25 21:35:45 +0100

    eu4: fix culture_stats.py
    add dev, production columns and remove non-working
    nation designer cost. The cost relied on the terrain which
    needs a terrain screenshot as input which made the script more difficult
    to use

--- scripts/eu4/culture_stats.py
+++ scripts/eu4/culture_stats.py
@@ -9,12 +9,11 @@ import pyradox
 
 
 
-import province_costs
 
 sources = ['EU4', 'text', 'nw2', 'res_publica', "aow"]
 
 def localized(s):
-    return pyradox.yml.get_localisation(s) or pyradox.format.human_title(s)
+    return pyradox.yml.get_localisation(s, 'EU4') or pyradox.format.human_title(s)
 
 
 culture_groups = {}
@@ -29,7 +28,7 @@ culture_data = {}
 culture_group_data = {}
 
 for filename, data in pyradox.txt.parse_dir(os.path.join(pyradox.get_game_directory('EU4'), 'history', 'provinces'), verbose=False):
-    data = data.at_date(pyradox.Date('1444.11.11'))
+    data = data.at_time(pyradox.Time('1444.11.11'))
     if 'culture' not in data:
         if 'base_tax' in data:
             print('No culture defined in %s.' % filename)
@@ -41,33 +40,40 @@ for filename, data in pyradox.txt.parse_dir(os.path.join(pyradox.get_game_direct
 
     culture = localized(culture)
     if culture not in culture_data:
-        culture_data[culture] = ['', 0, 0, 0, 0]
+        culture_data[culture] = ['', 0, 0, 0, 0, 0]
     culture_data[culture][0] = localized(culture_group)
     culture_group = localized(culture_group)
-    
+
     if culture_group not in culture_group_data:
-        culture_group_data[culture_group] = [0, 0, 0, 0]
-    
+        culture_group_data[culture_group] = [0, 0, 0, 0, 0]
+
     culture_data[culture][1] += 1
     culture_group_data[culture_group][0] += 1
-    
+
     if 'base_tax' in data:
+        culture_data[culture][3] += data['base_tax']
+        culture_group_data[culture_group][2] += data['base_tax']
         culture_data[culture][2] += data['base_tax']
         culture_group_data[culture_group][1] += data['base_tax']
-    if 'manpower' in data:
-        culture_data[culture][3] += data['manpower']
-        culture_group_data[culture_group][2] += data['manpower']
-    culture_data[culture][4] += province_costs.province_cost(data)
-    culture_group_data[culture_group][3] += province_costs.province_cost(data)
+    if 'base_production' in data:
+        culture_data[culture][4] += data['base_production']
+        culture_group_data[culture_group][3] += data['base_production']
+        culture_data[culture][2] += data['base_production']
+        culture_group_data[culture_group][1] += data['base_production']
+    if 'base_manpower' in data:
+        culture_data[culture][5] += data['base_manpower']
+        culture_group_data[culture_group][4] += data['base_manpower']
+        culture_data[culture][2] += data['base_manpower']
+        culture_group_data[culture_group][1] += data['base_manpower']
 
 result = ''
 
 result += '{|class = "wikitable sortable mw-collapsible mw-collapsed"\n'
-result += '! Culture group !! Province count !! Base tax !! Base manpower !! Cost\n'
+result += '! Culture group !! Province count !! Development !! Base tax !! Base production !! Base manpower\n'
 
 for culture_group, stats in sorted(culture_group_data.items()):
     result += '|-\n'
-    result += '| %s || %d || %d || %d || %d \n' % tuple([culture_group] + stats)
+    result += '| %s || %d || %d || %d || %d || %d \n' % tuple([culture_group] + stats)
 
 result += '|}\n'
 
@@ -76,11 +82,11 @@ print(result)
 result = ''
 
 result += '{|class = "wikitable sortable mw-collapsible mw-collapsed"\n'
-result += '! Culture !! Culture group !! Province count !! Base tax !! Base manpower !! Cost\n'
+result += '! Culture !! Culture group !! Province count !! Development !! Base tax !! Base production !! Base manpower\n'
 
 for culture, stats in sorted(culture_data.items()):
     result += '|-\n'
-    result += '| %s || %s || %d || %d || %d || %d\n' % tuple([culture] + stats)
+    result += '| %s || %s || %d || %d || %d || %d || %d\n' % tuple([culture] + stats)
 
 result += '|}\n'
 
