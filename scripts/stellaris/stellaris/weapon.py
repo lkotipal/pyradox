db6cb73cc27a49ba4688ce0ff871882dd0f9b401
grotaclas <grotaclas@gmail.com>
2024-10-29 21:12:13 +0100

    stellaris: update weapon script

--- scripts/stellaris/stellaris/weapon.py
+++ scripts/stellaris/stellaris/weapon.py
@@ -10,6 +10,7 @@ slot_sizes = {
     'extra_large' : 8,
     'titanic' : 16, #?
     'planet_killer' : 1,
+    'engulf': 0, # TODO: find correct size
     }
 
 slot_icons = {
@@ -29,8 +30,8 @@ def average_damage(weapon):
         min_damage = weapon['damage']['min']
         max_damage = weapon['damage']['max']
     else:
-        min_damage = weapon['min_damage']
-        max_damage = weapon['max_damage']
+        min_damage = weapon.find('min_damage', 0)
+        max_damage = weapon.find('max_damage', 0)
     
     return 0.5 * (min_damage + max_damage)
 
@@ -40,7 +41,7 @@ def normalized_dps(weapon):
     else:
         count = 1
         
-    dps_per_size = count * average_damage(weapon) / weapon['cooldown'] / slot_sizes[weapon['size'].lower()]
+    dps_per_size = count * average_damage(weapon) / weapon.find('cooldown', 0) / slot_sizes[weapon.find('size', 0).lower()]
     
     effectiveness_numerator = (
         0.5 * weapon['hull_damage'] +
@@ -60,32 +61,34 @@ def float_to_255(x):
 def to_html_color(c):
     return '#%02x%02x%02x' % c
 
-def special_color(key, weapon):
-    if weapon['armor_penetration'] == 1.0 and weapon['shield_penetration'] == 1.0:
+def special_color(key, ap, sp, hull_damage, armor_damage, shield_damage):
+    if ap == 1.0 and sp == 1.0:
         r = 1.0
         g = 0.0
         b = 1.0
     else:
         hull = 1.0
-        armor = weapon['armor_damage'] / weapon['hull_damage']
-        shield = weapon['shield_damage'] / weapon['hull_damage']
+        if hull_damage == 0:
+            hull_damage = 1  # to prevent division by 0
+        armor = armor_damage / hull_damage
+        shield = shield_damage / hull_damage
         
         non_penetrating = [hull]
-        if weapon['armor_penetration'] < 1.0:
+        if ap < 1.0:
             non_penetrating.append(armor)
-        if weapon['shield_penetration'] < 1.0:
+        if sp < 1.0:
             non_penetrating.append(shield)
 
         m = max(non_penetrating)
 
         power = 2.0
 
-        if weapon['armor_penetration'] < 1.0:
+        if ap < 1.0:
             r = 0.5 * (armor / m) ** power
         else:
             r = 1.0
         g = 0.5 * (hull / m) ** power
-        if weapon['shield_penetration'] < 1.0:
+        if sp < 1.0:
             b = 0.5 * (shield / m) ** power
         else:
             b = 1.0
@@ -95,8 +98,14 @@ def special_color(key, weapon):
 
 def special_description(key, weapon):
     hull = weapon['hull_damage']
+    if hull is None or hull == '':
+        hull = 0
     armor = weapon['armor_damage']
+    if armor is None or armor == '':
+        armor = 0
     shield = weapon['shield_damage']
+    if shield is None or shield == '':
+        shield = 0
 
     ap = weapon['armor_penetration']
     sp = weapon['shield_penetration']
@@ -116,7 +125,7 @@ def special_description(key, weapon):
     else:
         return ''
 
-    color = special_color(key, weapon)
+    color = special_color(key, ap, sp, hull, armor, shield)
 
     result = ''
     result += '<p style="color:%s; font-weight:bold; text-align:right; margin:0;">%s</p>' % (color, text)
@@ -126,6 +135,8 @@ def special_string(key, weapon):
     #items = [special_description(key, weapon)]
     items = []
 
+    if 'hull_damage' not in weapon:
+        return ''
     if weapon['hull_damage'] != 1.0:
         items.append('%d%% hull damage' % (weapon['hull_damage'] * 100.0))
 
@@ -150,16 +161,28 @@ def special_string(key, weapon):
     return result
     
 def is_missile(key, weapon):
+    if 'missile_health' not in weapon or not isinstance(weapon['missile_health'], (int, float)):
+        return False
     return weapon['missile_health'] > 0.0
 
 def slot_string(key, weapon):
     return '{{icon|slot %s}}' % slot_icons[weapon['size'].lower()]
 
 def icon_and_name(k, v):
-    tech = v['prerequisites'].replace('_', ' ')
     name = pyradox.get_localisation(k, game = 'Stellaris')
+    if 'icon' in v:
+        icon = v['icon'].removeprefix('GFX_')
+    elif 'prerequisites' in v:
+        icon = v['prerequisites'].replace('_', ' ')
+    else:
+        icon = None
 
-    return '[[File:%s.png|26px]] %s' % (tech, name)
+    if icon:
+        icon_tag = '[[File:%s.png|26px]]' % icon
+    else:
+        icon_tag = ''
+
+    return '%s %s' % (icon_tag, name)
     
 def icon_and_name_and_role(k, v):
     line_0 = icon_and_name(k, v)
--- scripts/stellaris/weapons.py
+++ scripts/stellaris/weapons.py
@@ -6,9 +6,8 @@ import math
 
 import stellaris.weapon
 
-csv_data = pyradox.csv.parse_file(['common',
-                                   'component_templates',
-                                   'weapon_components.csv'],
+csv_data = pyradox.csv.parse_merge(['common',
+                                   'component_templates'],
                                   game = 'Stellaris')
 
 txt_data = pyradox.parse_merge(['common',
@@ -19,18 +18,25 @@ total_data = pyradox.Tree()
 for weapon in txt_data.find_all('weapon_component_template'):
     if weapon['hidden']: continue
     key = weapon['key']
-    total_data[key] = weapon + csv_data[key]
+    weapon_csv = csv_data[key]
+    if weapon_csv['hull_damage'] in (None, '', '0.'):
+        weapon_csv['hull_damage'] = 0
+    if weapon_csv['armor_damage'] in (None, '', '0.'):
+        weapon_csv['armor_damage'] = 0
+    if weapon_csv['shield_damage'] in (None, '', '0.'):
+        weapon_csv['shield_damage'] = 0
+    total_data[key] = weapon + weapon_csv
 
 column_specs = [
     ('Weapon/Role', stellaris.weapon.icon_and_name_and_role),
     ('Size', stellaris.weapon.slot_string),
     ('{{icon|minerals}}<br/>Cost', '%(cost)d'),
-    ('{{icon|power}}<br/>Power', lambda k, v: str(abs(v['power']))),
+    ('{{icon|power}}<br/>Power', lambda k, v: str(abs(v.find('power', 0)) if v.find('power', 0) != '' else '')),
     ('{{icon|damage}}<br/>Average<br/>damage', lambda k, v: '%0.1f' % stellaris.weapon.average_damage(v)),
     ('{{icon|time}}<br/>Cooldown', '%(cooldown)d'),
     ('{{icon|weapons range}}<br/>Range', '%(range)d'),
-    ('{{icon|ship accuracy}}<br/>Accuracy', lambda k, v: '%d%%' % (v['accuracy'] * 100.0)),
-    ('{{icon|tracking}}<br/>Tracking', lambda k, v: '%d%%' % (v['tracking'] * 100.0)),
+    ('{{icon|ship accuracy}}<br/>Accuracy', lambda k, v: '%d%%' % (v.find('accuracy', 0) * 100.0)),
+    ('{{icon|tracking}}<br/>Tracking', lambda k, v: '%d%%' % (v.find('tracking', 0) * 100.0)),
     ('{{icon|damage}}/{{icon|time}}<br/>Normalized DPS', lambda k, v: '%0.2f' % stellaris.weapon.normalized_dps(v)),
     ('Modifiers', stellaris.weapon.special_string),
     ]
@@ -39,7 +45,7 @@ missile_specs = [
     ('Weapon/Role', stellaris.weapon.icon_and_name_and_role),
     ('Size', stellaris.weapon.slot_string),
     ('{{icon|minerals}}<br/>Cost', '%(cost)d'),
-    ('{{icon|power}}<br/>Power', lambda k, v: str(abs(v['power']))),
+    ('{{icon|power}}<br/>Power', lambda k, v: str(abs(v.find('power', 0)) if v.find('power', 0) != '' else '')),
     ('{{icon|damage}}<br/>Average<br/>damage', lambda k, v: '%0.1f' % stellaris.weapon.average_damage(v)),
     ('{{icon|time}}<br/>Cooldown', '%(cooldown)d'),
     ('{{icon|weapons range}}<br/>Range', '%(range)d'),
@@ -54,10 +60,10 @@ missile_specs = [
     #('Shield', '%(missile_shield).1f'),
     ]
 
-with open('out/weapons.wiki', 'w') as outfile:
+with open('out/weapons.wiki.txt', 'w') as outfile:
     outfile.write(pyradox.filetype.table.make_table(total_data, 'wiki', column_specs))
 
-with open('out/weapons_split.wiki', 'w') as outfile:
+with open('out/weapons_split.wiki.txt', 'w') as outfile:
     outfile.write(pyradox.filetype.table.make_tables(
         total_data, 'wiki',
         column_specs = column_specs,
@@ -66,7 +72,7 @@ with open('out/weapons_split.wiki', 'w') as outfile:
         sort_function = stellaris.weapon.sort_function,
         table_classes = ["wikitable sortable"])) 
 
-with open('out/missiles.wiki', 'w') as outfile:
+with open('out/missiles.wiki.txt', 'w') as outfile:
     outfile.write(pyradox.filetype.table.make_table(
         total_data, 'wiki',
         missile_specs,
